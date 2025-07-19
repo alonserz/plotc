411c6770d265ded92396259ac1dfc17f42220df3
alonserz <59419908+alonserz@users.noreply.github.com>
2025-07-20 00:35:32 +0300

    just use buffers

--- src/plotc.c
+++ src/plotc.c
@@ -21,12 +21,6 @@ struct Tuple get_min_max(double *array, int size){
     struct Tuple tuple = {min, max};
     return tuple;
 }
-bool is_inarray(double value, double *array, int size){
-    for(int i = 0; i < size; i++){
-	if(array[i] == value) return true;
-    }
-    return false;
-}
 
 double scale_in_range_01(double x, double a, double b){
     return (x - a)/(b - a);
@@ -50,33 +44,39 @@ void show_scatter_plot(double *x, int size_x, double *y, int size_y, int term_wi
 
 
     for(int i = 0; i < size_y; i++){
-	scaled_x[i] = floor(scale_in_range_01(x[i], tuple_x.min, tuple_x.max) * term_width);
+	scaled_x[i] = (int)(scale_in_range_01(x[i], tuple_x.min, tuple_x.max) * term_width);
     }
     for(int i = 0; i < size_y; i++){
-	scaled_y[i] = floor(scale_in_range_01(y[i], tuple_y.min, tuple_y.max) * term_height);
+	scaled_y[i] = (int)(scale_in_range_01(y[i], tuple_y.min, tuple_y.max) * term_height);
     }
 
-    int scaled_x_size = sizeof(scaled_x) / sizeof(scaled_x[0]);
-    // int scaled_y_size = sizeof(scaled_y) / sizeof(scaled_y[0]);
+    // int scaled_x_size = sizeof(scaled_x) / sizeof(scaled_x[0]);
+    int scaled_y_size = sizeof(scaled_y) / sizeof(scaled_y[0]);
+    char row_buffer[term_width + 1];
     
     for(int i = term_height; i > -1; i--){
-	if(is_inarray(i, scaled_y, scaled_x_size)){
-	    for(int j = 0; j < term_width + 1; j++){
-		int index = get_index_by_value(j, scaled_x, scaled_x_size);
-		double y_value = scaled_y[index];
-		if(y_value == i){
-		    printf("\x1b[1;31;41m*");
-		}
-		else{
-		    printf("\x1b[1;30;40m*");
-		}
-	    }
+	for(int j = 0; j < term_width + 1; j++){
+	    row_buffer[j] = ' ';
 	}
-	else{
-	    for(int j = 0; j < term_width + 1; j++){
-		printf("\x1b[1;30;40m*");
+	row_buffer[term_width + 1] = '\n';
+
+	for(int j = 0; j < scaled_y_size; j++){
+	    if(!(scaled_y[j] == i)){
+		continue;
 	    }
+	    int x_value = (int)scaled_x[j];
+	    if (x_value < term_width + 1) {
+		row_buffer[x_value] = '*';
+            }
 	}
+
+	for (int j = 0; j < term_width + 1; j++) {
+            if (row_buffer[j] == '*') {
+                printf("\x1b[1;31;41m*");
+            } else {
+                printf("\x1b[1;30;40m*");
+            }
+        }
 	printf("\x1b[1;39;49m\n");
     }
 }
@@ -84,13 +84,13 @@ void show_scatter_plot(double *x, int size_x, double *y, int size_y, int term_wi
 double* range(int a, int b, int n);
 
 int main(){
-    double x[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    double y[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    double x[] = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
+    double y[] = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
     int x_size = sizeof(x) / sizeof(x[0]);
     int y_size = sizeof(y) / sizeof(y[0]);
 
-    int term_width = 110;
-    int term_height = 20;
+    int term_width = 120;
+    int term_height = 30;
 
     show_scatter_plot(x, x_size, y, y_size, term_width, term_height);
     return 69;
